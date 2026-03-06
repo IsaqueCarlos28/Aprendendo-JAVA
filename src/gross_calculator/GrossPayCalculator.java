ceddf4fb1f17c077bb250377b675800a83489cbe
Isaque Carlos da Silva Melo <isaquecarlos2016@gmail.com>
2026-03-06 12:23:53 -0300

    Swiching the notes to a Tutorial package

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -1,21 +1,7 @@
-/*Packages are folders/conteiners that contain java files
-used for logical grouping of java files*/
-//Declaration of the package this class belongs to, it happens in the beginning of the file.
-//Convention: packages names must be in lower case separated by underscore("_")
 package gross_calculator;
 
-//Java files are called Classes (OOP)
-/*Declaration of the class and start of the Java class
-the code is built within the braces ("{}")*/
-/*Convention: class names must have no separation between words and each word
-must begin with a capital letter*/
 public class GrossPayCalculator {
-    //In order to run any code within a java class they must be in a method
-    /*This is the main method, it's the first method within the class to run
-    when the class is called */
-    //Declaration of the main Method
     public static void main(String[] args){
-        //Code to show a message on the console
         System.out.println("Hello World");
     }
 }
