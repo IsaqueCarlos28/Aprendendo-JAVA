43bd0b51d030c5e30af601ca582529cc7c76b512
Isaque Carlos da Silva Melo <isaquecarlos2016@gmail.com>
2026-03-06 11:43:19 -0300

    creating first package and class

---
+++ src/gross_calculator/GrossPayCalculator.java
@@ -0,0 +1,7 @@
+// Declaration of the package this class belongs to.
+//Convention: packages names must be in lower case separated by underscore("_")
+package gross_calculator;
+
+
+public class GrossPayCalculator {
+}
