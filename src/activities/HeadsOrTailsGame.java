b45b1974e5f37b65c07372f68fb08f291d83f2cb
Isaque Carlos da Silva Melo <isaquecarlos2016@gmail.com>
2026-03-07 13:25:14 -0300

    Repetition Structures

---
+++ src/activities/HeadsOrTailsGame.java
@@ -0,0 +1,53 @@
+package activities;
+
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HeadsOrTailsGame {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            //player input
+            System.out.println("Choose heads: 1 or tails: 2");
+            int playerChoice = scanner.nextInt();
+            if (playerChoice != 1 && playerChoice != 2){
+                System.out.println("Choose a valid option: Heads -> 1 OR Tails -> 2");
+                playerChoice = scanner.nextInt();
+            }
+
+            //run game
+            System.out.println("The coin is tossed ...");
+            Random random = new Random();
+            int headOrTail = random.nextInt(2) + 1;
+
+            //Show result to player
+            String resultGame = switch (headOrTail){
+              case 1 -> "Heads!";
+              case 2 -> "Tails!";
+              default -> "Error!";
+            };
+            if (headOrTail == playerChoice){
+                System.out.println(resultGame + " Congratulations you won");
+            }else{
+                System.out.println(resultGame + " You lost");
+            }
+
+            //play again
+            //clears the scanner because I used int last time
+            scanner.nextLine();
+            System.out.println("Do you wanna play again? -> YES: Y or NO: N");
+            String playAgain = scanner.nextLine();
+            //verify if the strings are valid
+            if (!playAgain.equalsIgnoreCase("Y") && !playAgain.equalsIgnoreCase("N")){
+                System.out.println("Choose a valid option: -> YES: Y or NO: N");
+                playAgain = scanner.nextLine();
+            }
+
+            if (Objects.equals(playAgain, "N")) break;
+        }
+    }
+}
---
+++ src/tutorials/RepetitionStructures.java
@@ -0,0 +1,34 @@
+package tutorials;
+
+public class RepetitionStructures {
+    public static void main(String[] args){
+        //WHILE LOOP
+        //Loops a code while a condition is true
+        /*the condition is tested before the loop is started,
+        witch means the loop can never run*/
+        int x = 1;
+        while (x <= 10){
+            System.out.println(x + " - Condition True");
+            x = x + 1;//the variable is updated to prevent a infinite loop
+        }
+
+        // DO WHILE LOOP
+        //Loops a code while a condition is true
+        /*the condition is tested after the loop is started,
+        witch means the loop will run at least once*/
+        int y = 1;
+        do {
+            System.out.println("the loop is run");
+        }while(y < 1);
+
+        // DO WHILE LOOP
+        /*the loop is controlled by a count with a defined number of times or until
+        conditional max number is met*/
+        for (int i = 1;i <= 10;i++){
+            System.out.println("counting: " + i);
+        }
+
+        //BREAK LOOP
+        //To exit a loop even if the condition is still true, use the reserved word "break"
+    }
+}
