5a0af099075d26513e0e18390e1929cf0b7309dd
Isaque Carlos da Silva Melo <isaquecarlos2016@gmail.com>
2026-03-09 11:28:31 -0300

    arrays

--- src/tutorials/Arrays.java
+++ src/tutorials/Arrays.java
@@ -1,6 +1,27 @@
 package tutorials;
 
 public class Arrays {
+    public static void main(String[] args){
     //ARRAYS
-    //are data types that function as a conteiner for various values
+    //  Are data types that function as a container for various values of the same type
+    //  An array is declared by brackets("[]") after the type or the name
+    //  String[] arrayName = new String[6]
+    //  String arrayName[] = new String[6];
+    //  The number inside the type indicates how many values the array will storage
+    //  The quantity of space of the array is fixed, so it can't lengthen to hold more values
+        String[] names = new String[5];
+        names[0] = "Zhao";
+        names[1] = "Zuko";
+        names[2] = "Long Fang";
+        names[3] = "Azula";
+        names[4] = "Ozai";
+
+    /* If the values of the array are already know, it's possible to create an array with
+    the length of the values quantity of values*/
+        String[] names2 = {"Amon","Unalaq","Zaheer","Kuvira"};
+
+    //The values of an Array are accessed by the index of its position in the array
+        System.out.println(names[0] + " and "+names2[0]+
+                " Are the first villains of each series of Avatar");
+    }
 }
