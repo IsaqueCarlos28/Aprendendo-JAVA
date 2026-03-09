1395a830fa15fe34f800288292f0bfb4550296c5
Isaque Carlos da Silva Melo <isaquecarlos2016@gmail.com>
2026-03-09 16:34:40 -0300

    improving the StrengthOfPassword

--- src/activities/StrengthOfPassword.java
+++ src/activities/StrengthOfPassword.java
@@ -1,44 +1,36 @@
 package activities;
 import java.util.Scanner;
 
-public class StrengthOfPassword {
+public class StrengthOfPassword{
 
     // Current credentials
-    static String username = "jane_doe";
-    static String currentPassword = "passw0rd";
-
-    /*
-     - 8 characters long
-     - contains a UPPERCASE letter
-     - contains an especial character
-     - does not contain the username
-     - is not the same password
-     */
+    static final String USERNAME = "jane_doe";
+    static final String CURRENT_PASSWORD = "passw0rd";
 
     public static void main(String[] args){
         System.out.println("""
                 Choose your new password,it must:
                 - Be at least 8 characters long;
                 - Contain a UPPERCASE letter;
-                - Contain an especial character;
+                - Contain an special character;
                 - Not contain the username;
                 - Not be the same as the current password;
                 """);
         Scanner scanner = new Scanner(System.in);
-
-        String newPassword = scanner.nextLine();
-        boolean isPasswordValid = validatePassword(newPassword);
-        if (isPasswordValid){
+        String newPassword;
+        boolean isPasswordValid;
+        do{
+            newPassword = scanner.nextLine();
+            isPasswordValid = validatePassword(newPassword);
+            if (!isPasswordValid){
+            System.out.println("\nTry again");
+            }
+        }while(!isPasswordValid);
             System.out.print("You changed your password");
-        }else {
-            System.out.print(
-"Try again");
-        }
-
+            scanner.close();
     }
 
     static boolean validatePassword(String newPassword) {
-
         boolean isPasswordValid = true;
         StringBuilder passwordStatus = new StringBuilder();
         if (newPassword.length() < 8){
@@ -50,19 +42,21 @@ public class StrengthOfPassword {
             isPasswordValid = false;
         }
         if (newPassword.matches("[a-zA-Z0-9]*")) {
-            passwordStatus.append("\nYour password must contain at least one especial character");
+            passwordStatus.append("\nYour password must contain at least one special character");
             isPasswordValid = false;
         }
-        if (newPassword.contains(username)){
+        if (newPassword.contains(USERNAME)){
             passwordStatus.append("\nYour password must not contain the username");
             isPasswordValid = false;
         }
-        if (newPassword.equals(currentPassword)){
+        if (newPassword.equals(CURRENT_PASSWORD)){
             passwordStatus.append("\nYour password must not be the same as the current password");
             isPasswordValid = false;
         }
 
-        System.out.println(passwordStatus.toString());
+        if(!isPasswordValid){
+            System.out.println(passwordStatus);
+        }
         return isPasswordValid;
     }
 }
